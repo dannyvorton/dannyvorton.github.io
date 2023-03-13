a578c8102e5841ca52275bb889921955d69431f6
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2023-03-13 16:31:08 -0600

    working with linkedin profile badge

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// LINK TO LINKEDIN BADGE
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+
+// LINKEDIN BADGE
+    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="orton" data-version="v1">
+        <a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/orton?trk=profile-badge">Danny Orton</a>
